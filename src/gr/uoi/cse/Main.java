3a63f49ef8786f89df436102fdd4f587741ad436
zakemk <xar.zax@hotmail.com>
2023-05-29 19:21:59 +0300

    moved static methods from Main.java to an object called ETLFacade

--- src/gr/uoi/cse/Main.java
+++ src/gr/uoi/cse/Main.java
@@ -1,53 +1,12 @@
 package gr.uoi.cse;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.List;
-
-import gr.uoi.cse.classcollector.ClassCollector;
-import gr.uoi.cse.etl.ETL;
-import gr.uoi.cse.parser.Parser;
-import gr.uoi.cse.writer.DiskWriter;
+import gr.uoi.cse.etl.ETLFacade;
 
 public final class Main
 {
-	private static final String PROJECT_BASE_PATH = "gr.uoi.cse";
-	
 	public static void main (String ... args)
 	{
-		performEtlForAllClasses();
-	}
-	
-	private static final void performEtlForAllClasses()
-	{
-		final ClassCollector classCollector = new ClassCollector();
-		final List<Class<?>> etlClassList = classCollector
-				.collectClasses(PROJECT_BASE_PATH)
-				.stream()
-				.filter(c -> c.isAnnotationPresent(ETL.class))
-				.toList();
-		
-		try
-		{
-			for (final Class<?> etlClass : etlClassList)
-				performEtl(etlClass);
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-	}
-	
-	private static final void performEtl(Class<?> etlClass) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException
-	{
-		final ETL etlAnnotation = etlClass.getAnnotation(ETL.class);
-		final String parseDirectory = etlAnnotation.parseDirectory();
-		final String outputDirectory = etlAnnotation.outputDirectory();
-		final Class<? extends Parser<?>> parserClass = etlAnnotation.parserClass();
-		final Parser<?> parser = parserClass.getDeclaredConstructor().newInstance();
-		final List<?> entityList = parser.parseDocument(parseDirectory);
-		
-		final DiskWriter diskWriter = new DiskWriter();
-		diskWriter.writeToDisk(entityList, outputDirectory);
+		final ETLFacade etlFacade = new ETLFacade();
+		etlFacade.performETL();
 	}
 }
--- src/gr/uoi/cse/etl/ETLFacade.java
+++ src/gr/uoi/cse/etl/ETLFacade.java
@@ -1,7 +1,46 @@
 package gr.uoi.cse.etl;
 
-@FunctionalInterface
-public interface ETLFacade
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+import gr.uoi.cse.classcollector.ClassCollector;
+import gr.uoi.cse.parser.Parser;
+import gr.uoi.cse.writer.DiskWriter;
+
+public final class ETLFacade
 {
-	void performETL();
+	private static final String PROJECT_BASE_PATH = "gr.uoi.cse";
+	
+	public void performETL()
+	{
+		final ClassCollector classCollector = new ClassCollector();
+		final List<Class<?>> etlClassList = classCollector
+				.collectClasses(PROJECT_BASE_PATH)
+				.stream()
+				.filter(c -> c.isAnnotationPresent(ETL.class))
+				.toList();
+		
+		try
+		{
+			for (final Class<?> etlClass : etlClassList)
+				performEtl(etlClass);
+		}
+		catch (final Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	private final void performEtl(Class<?> etlClass) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException
+	{
+		final ETL etlAnnotation = etlClass.getAnnotation(ETL.class);
+		final String parseDirectory = etlAnnotation.parseDirectory();
+		final String outputDirectory = etlAnnotation.outputDirectory();
+		final Class<? extends Parser<?>> parserClass = etlAnnotation.parserClass();
+		final Parser<?> parser = parserClass.getDeclaredConstructor().newInstance();
+		final List<?> entityList = parser.parseDocument(parseDirectory);
+		
+		final DiskWriter diskWriter = new DiskWriter();
+		diskWriter.writeToDisk(entityList, outputDirectory);
+	}
 }
