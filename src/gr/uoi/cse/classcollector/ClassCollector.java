705cc3c2b072301a668df93805edec6beccb493a
zakemk <xar.zax@hotmail.com>
2023-05-30 14:57:39 +0300

    removed 2 unused sql files

--- src/gr/uoi/cse/classcollector/ClassCollector.java
+++ src/gr/uoi/cse/classcollector/ClassCollector.java
@@ -6,54 +6,74 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
-public class ClassCollector{
-	
-    public List<Class<?>> collectClasses(String packageName) {
+public class ClassCollector
+{
+    public List<Class<?>> collectClasses(String packageName) 
+    {
         final List<Class<?>> classList = new ArrayList<>();
 
-        try {
+        try 
+        {
             final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
             final String packagePath = packageName.replace('.', '/');
             final Enumeration<URL> resources = classLoader.getResources(packagePath);
 
-            while (resources.hasMoreElements()) {
+            while (resources.hasMoreElements()) 
+            {
                 final URL resource = resources.nextElement();
                 final File file = new File(resource.toURI());
 
-                if (file.isDirectory()) {
+                if (file.isDirectory()) 
+                {
                     collectClassesFromDirectory(file, packageName, classList);
-                } else if (file.getName().endsWith(".class")) {
+                }
+                else if (file.getName().endsWith(".class")) 
+                {
                     final String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
 
-                    try {
+                    try
+                    {
                         final Class<?> clazz = Class.forName(className);
                         classList.add(clazz);
-                    } catch (ClassNotFoundException e) {
+                    } 
+                    catch (ClassNotFoundException e)
+                    {
                         e.printStackTrace();
                     }
                 }
             }
-        } catch (final Exception e) {
+        } 
+        catch (final Exception e) 
+        {
             e.printStackTrace();
         }
 
         return classList;
     }
 
-    private void collectClassesFromDirectory(File directory, String packageName, List<Class<?>> classList) {
+    private void collectClassesFromDirectory(File directory, String packageName, List<Class<?>> classList)
+    {
         final File[] files = directory.listFiles();
 
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
+        if (files != null) 
+        {
+            for (File file : files) 
+            {
+                if (file.isDirectory())
+                {
                     collectClassesFromDirectory(file, packageName + '.' + file.getName(), classList);
-                } else if (file.getName().endsWith(".class")) {
+                } 
+                else if (file.getName().endsWith(".class"))
+                {
                     String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
 
-                    try {
+                    try 
+                    {
                         final Class<?> clazz = Class.forName(className);
                         classList.add(clazz);
-                    } catch (final ClassNotFoundException e) {
+                    } 
+                    catch (final ClassNotFoundException e) 
+                    {
                         e.printStackTrace();
                     }
                 }
